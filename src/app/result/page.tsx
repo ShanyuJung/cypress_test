9de1d9f5d317dad8625a292a7eb26308b3e9213a
Luka <srps8912@gmail.com>
2023-07-15 00:56:07 +0800

    feat: Update result

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import classes from "./page.module.css";
+
 const Result = () => {
-  return <div>Result</div>;
+  const searchParams = useSearchParams();
+
+  const firstName = searchParams.get("firstName");
+  const lastName = searchParams.get("lastName");
+
+  return (
+    <div className={classes.container}>
+      <div>{`First Name: ${firstName}`}</div>
+      <div>{`First Name: ${lastName}`}</div>
+    </div>
+  );
 };
 
 export default Result;
