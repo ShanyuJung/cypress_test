663326fbb003aa62e2c99f8c33d5fe3f80d347b8
Luka <srps8912@gmail.com>
2023-07-24 00:29:22 +0800

    test: Update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
     viewportHeight: 1080,
     viewportWidth: 1920,
-    video: false,
-    screenshotOnRunFailure: false,
   },
+  video: true,
+  screenshotOnRunFailure: true,
 });
