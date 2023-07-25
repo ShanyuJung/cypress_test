4c9b2f1950a083169bb06c6239417ed36a85d47e
Luka <srps8912@gmail.com>
2023-07-26 01:21:53 +0800

    feat: add env

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "cypress";
 
 const { combinedEnv } = loadEnvConfig(process.cwd());
 export default defineConfig({
-  env: combinedEnv,
+  env: {
+    DEV_API_KEY: combinedEnv.DEV_API_KEY,
+    TEST_API_KEY: combinedEnv.TEST_API_KEY,
+    PROD_API_KEY: combinedEnv.PROD_API_KEY,
+  },
   e2e: {
     baseUrl: "http://localhost:3000",
     retries: {
